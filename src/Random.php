2d1db267f01e8e4339a76ebeeb15378453996fe2
Ahmad Susanto <ahmadsusanto912@gmail.com>
2022-11-26 22:15:42 +0700

    Versi 2.0.0

--- src/Random.php
+++ src/Random.php
@@ -6,7 +6,11 @@ class Random
 {
     public static function getNumber()
     {   
-        // return rand(123000, 1000000);
+        return rand(100000000000, 1100000000000000);
+    }
+
+    public static function getString()
+    {   
         return vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(16)), 4) );
     }
 }
